a6930c959384094da43757ec6baf553d4487f0e2
Matt Greene <m.greene@f5.com>
2016-08-15 09:23:11 -0600

    Updated script to remap fields from launchpad into fault genes template and generate a csv report

--- tools/bugdump.py
+++ tools/bugdump.py
@@ -1,36 +1,33 @@
-import json
+import csv
 from launchpadlib.launchpad import Launchpad
-import os
+from pprint import pprint
 
 
-class BugDump(object):
-    def __init__(self, debug=False, limit=False, count=5):
-        self.limit = limit
-        self.count = count  # Stop after N bugs if limit = True
+class Report(object):
+    default_filepath = 'reports/bugdump.csv'
+    default_project = 'openstack'
+    default_importance = [
+        'Critical'
+    ]
+    default_status = [
+        'New', 'Incomplete', 'In Progress', 'Confirmed', 'Triaged',
+        'Fix Committed'
+    ]
+
+    def __init__(self, debug=False, limit=None):
+        self.limit = limit # Stop after 'limit' bugs if not None
         self.debug = debug
+        self.dialect = csv.excel
+        self.report = []
         if self.debug:
             print("Enabling debug output")
         if self.limit:
-            print("Limiting report to %d bugs" % self.count)
+            print("Limiting report to %d bugs" % self.limit)
 
-    def meta_keys(self):
-        keys = [
-            'assignee_link',
-            'bug_link',
-            'bug_target_display_name',
-            'bug_target_name',
-            'date_created',
-            'importance',
-            'owner_link',
-            'related_tasks_collection_link',
-            'self_link',
-            'status',
-            'target_link',
-            'title',
-            'web_link'
-        ]
-        '''
-        unused_keys = [
+    def get_meta_keys(self):
+        # Unused, but this is the list of all fields present in a 'task'.
+        # A 'task' contain meta-data encapsulating a bug.
+        return [
             'http_etag',
             'is_complete',
             'milestone_link',
@@ -46,51 +43,139 @@ class BugDump(object):
             'date_left_closed',
             'date_left_new',
             'date_triaged',
+            'assignee_link',
+            'bug_link',
+            'bug_target_display_name',
+            'bug_target_name',
+            'date_created',
+            'importance',
+            'owner_link',
+            'related_tasks_collection_link',
+            'self_link',
+            'status',
+            'target_link',
+            'title',
+            'web_link'
         ]
-        '''
-        return keys
 
-    def report(self, project='neutron'):
-        cachedir = os.path.join(os.environ['HOME'], 'Code/launchpad/cache')
+    def get_ordered_fieldnames(self):
+        return [
+            'req',
+            'bug',
+            'version',
+            'project',
+            'component',
+            'fault_class',
+            'fault_type',
+            'fault_description',
+            'fault_symptom',
+            'severity',
+            'priority',
+            'status',
+            'mitigation',
+            'log',
+            'repro',
+            'submitter',
+            'assignee',
+            'created',
+            'deployment'
+        ]
+
+    def xstr(self, string):
+        if string is None:
+            return ''
+        else:
+            return string.encode('utf8')
+
+    def create(self, project, importance, status):
+        iteration = 0
+        self.report = []
         lp = Launchpad.login_anonymously('just testing', 'production',
-                                         cachedir, version='devel')
+                                         'cache', version='devel')
         p = lp.projects[project]
-        '''
-        status = [
-            'New', "Won't Fix", 'Incomplete', 'Opinion', 'Expired',
-            'Confirmed', 'Triaged', 'Fix Committed', 'Fix Released',
-            'Incomplete (with response)', 'Incomplete (without response)']
-        '''
         # pass status to filter on a subset of bugs
-        bug_tasks = p.searchTasks()
-        table = {}
-        meta_keys = self.meta_keys()
-        print("Found %d bugs in project %s" % (len(bug_tasks), project))
+        bug_tasks = p.searchTasks(importance=importance, status=status)
+        print("Found %d bugs in the %s project with importance %s" %
+              (len(bug_tasks), project, str(importance)))
         for task in bug_tasks:
+            if self.limit:
+                self.limit -= 1
             bug = lp.bugs.getBugData(bug_id=task.bug.id)[0]
             if self.debug:
-                print("Found data for %s" % task)
+                print "Processing %s" % task
             else:
-                print '.',
-            table[task.bug.id] = {}
-            table[task.bug.id]['details'] = bug
-            table[task.bug.id]['meta'] = {}
-            for key in meta_keys:
-                table[task.bug.id]['meta'][key] = getattr(task, key)
-                if key == 'date_created':
-                    # need to render this field as a string
-                    table[task.bug.id]['meta'][key] = \
-                        str(table[task.bug.id]['meta'][key])
-            if self.limit:
-                if self.count == 0:
-                    break
-                self.count -= 1
+                iteration += 1
+                (q, r) = divmod(iteration, 10)
+                print '%s' % iteration if r == 0 else '.',
+            # remap fields from lb task and bug
+            empty = ''
+            row = {
+                'req':                  self.xstr(bug['bug_summary']),
+                'bug':                  self.xstr(task.web_link),
+                'version':              empty,
+                'project':              self.xstr(task.bug_target_name),
+                'component':            empty,
+                'fault_class':          empty,
+                'fault_type':           empty,
+                'fault_description':    self.xstr(bug['description']),
+                'fault_symptom':        empty,
+                'severity':             self.xstr(task.importance),
+                'priority':             empty,
+                'status':               self.xstr(task.status),
+                'mitigation':           empty,
+                'log':                  empty,
+                'repro':                empty,
+                'submitter':            self.xstr(task.owner_link),
+                'assignee':             self.xstr(task.assignee_link),
+                'created':              self.xstr(str(task.date_created)),
+                'deployment':           empty
+            }
+            self.report.append(row)
+            if self.limit == 0:
+                break
         print("\nFinished compiling table for project %s" % project)
-        return table
 
+    def dump(self):
+        for row in self.report:
+            pprint(row)
+
+    def save(self, filepath):
+        with open(filepath, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile,
+                                    fieldnames=self.get_ordered_fieldnames(),
+                                    dialect=self.dialect)
+            writer.writeheader()
+            for row in self.report:
+                try:
+                    writer.writerow(row)
+                except Exception as err:
+                    print 'Row:'
+                    pprint(row)
+                    print 'Exception: %s' % err
+
+        csvfile.close()
+
+    def open(self, filepath):
+        self.report = []
+        with open(filepath, 'rb') as csvfile:
+            reader = csv.DictReader(csvfile, dialect=self.dialect)
+            for row in reader:
+                self.report.append(row)
+        csvfile.close()
+
+
+def generate_report(filepath=Report.default_filepath,
+                    project=Report.default_project,
+                    importance=Report.default_importance,
+                    status=Report.default_status):
+    report = Report(debug=False, limit=None)
+    report.create(project, importance, status)
+    report.save(filepath)
+
+def read_report(filepath):
+    report = Report(debug=False, limit=None)
+    report.open(filepath)
+    report.dump()
 
 if __name__ == "__main__":
-    b = BugDump(debug=True, limit=True, count=5)
-    bugs = b.report()
-    with open('bugdump.json', 'w') as fp:
-        json.dump(bugs, fp)
+    generate_report(importance=['High'])
